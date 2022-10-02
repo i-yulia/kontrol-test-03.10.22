d6a6346b3bf49ca913749d13353229707eb72cb7
yulia <kimkaaa@mail.ru>
2022-10-03 01:02:10 +0300

    Программа, решающая задачу

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+//длина которых меньше, либо равна 3 символам. 
+string[] FindLenght(string[] array)
+{
+    string[] result = new string[0];
+
+    for (int i = 0, j = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            Array.Resize(ref result, result.Length + 1);
+            result[j] = array[i];
+            j++;
+        }
+    }
+    return result;
+}
+Console.Write("Введите строки массива, разделив их запятой: ");
+string text = Console.ReadLine() ?? String.Empty;
+string[] myArray = text.Split(",");
+string[] newArray = FindLenght(myArray);
+Console.WriteLine($"Строки, длина которых не более 3 символов: {String.Join(", ", newArray)}");
